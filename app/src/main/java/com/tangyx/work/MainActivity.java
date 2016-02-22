ab63270fbc263eb38e1007548eae3497561bf3ed
tangyx <tangyx@live.com>
2016-02-22 15:52:51 +0800

    倒计时

--- app/src/main/java/com/tangyx/work/MainActivity.java
+++ app/src/main/java/com/tangyx/work/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
+            
             super.onBackPressed();
         }
     }
